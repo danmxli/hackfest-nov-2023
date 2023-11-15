f24756bfba77949e143a5721e856004e743130dd
danmxli <danmuxingli@gmail.com>
2023-11-15 11:40:00 -0500

    loading animation for initial auth0 fetch

--- frontend/components/UserPortal.tsx
+++ frontend/components/UserPortal.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
 interface PortalPhases {
     [key: string]: React.ReactNode;
@@ -48,17 +49,16 @@ const UserPortal = () => {
 
     return (
         <div>
-
             {user ? (
                 <div className="p-8 bg-teal-950 border border-2 border-teal-600 rounded-3xl m-4 text-teal-600">
-                    <h1 className='text-xl'>Welcome back, {user.name}</h1>
-                    <div className='mt-2 grid grid-cols-2 gap-2 justify-center items-center'>
-                        <div className='border p-2 rounded-lg text-center'>
+                    <h1 className='text-2xl text-center text-teal-400'>Welcome, {user.name}</h1>
+                    <div className='mt-6 grid grid-cols-2 gap-2'>
+                        <div className='border border-teal-900 hover:border-teal-600 p-3 rounded-3xl text-center flex items-center justify-center'>
                             User data
                         </div>
                         <button>
                             <div
-                                className='border p-2 rounded-lg text-center'
+                                className='border border-teal-900 hover:border-teal-600 p-3 rounded-3xl text-center flex items-center justify-center'
                                 onClick={loadUserAccess}
                             >
                                 Go to plan interface
@@ -68,13 +68,23 @@ const UserPortal = () => {
                     </div>
                 </div>
             ) : (
-                <a href="/api/auth/login">
-                    <div className="p-8 bg-teal-950 border border-2 border-teal-600 rounded-3xl m-4 text-teal-600 text-center">
-                        Login
-                    </div>
-                </a>
+                <div className="p-8 bg-teal-950 border border-2 border-teal-600 rounded-3xl m-4 text-teal-600 text-center">
+                    <h1 className='text-2xl text-teal-400'>Develop your ideas from the ground up.</h1>
+                    {isLoading ? (
+                        <div className='mt-6 p-2 rounded-3xl border border-teal-900'>
+                            <div className='flex items-center justify-center gap-2'>
+                                <AiOutlineLoading3Quarters className="animate-spin" />Loading user details
+                            </div>
+                        </div>
+                    ) : (
+                        <a href="/api/auth/login">
+                            <div className='mt-6 p-2 rounded-3xl border border-teal-900 hover:border-teal-600'>
+                                Click to login
+                            </div>
+                        </a>
+                    )}
+                </div>
             )}
-
         </div>
     )
 }
