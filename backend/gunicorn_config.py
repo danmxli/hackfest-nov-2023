020daa5c74894daf8160d1152c7bd25afb074a0a
danmxli <danmuxingli@gmail.com>
2023-11-22 16:43:20 -0500

    update timeout

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -5,7 +5,7 @@ workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 
-timeout = int(os.environ.get('GUNICORN_TIMEOUT', '150'))
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '180'))
 
 
 forwarded_allow_ips = '*'
